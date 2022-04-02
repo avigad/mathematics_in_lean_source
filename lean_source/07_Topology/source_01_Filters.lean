7af208f6cd923b6f3e402ec1c0bc589c28b35f76
Patrick Massot <patrickmassot@free.fr>
2022-04-02 15:30:47 +0200

    Apply suggestions from code review
    Co-authored-by: Julian Berman <Julian@GrayVines.com>

--- lean_source/07_Topology/source_01_Filters.lean
+++ lean_source/07_Topology/source_01_Filters.lean
@@ -14,22 +14,22 @@ Filters
 Filters on a type ``X`` are collections of sets of ``X`` satisfying three conditions. They are mostly used to
 abstract two related kinds of ideas:
 
-* *limits*, including including all kinds of limites discussed above, in particular finite or infinite limits of sequences, finite or infinite limits of functions at a point or at infinity, etc...
+* *limits*, including all kinds of limits discussed above, in particular finite or infinite limits of sequences, finite or infinite limits of functions at a point or at infinity, etc...
 
 * *things happening eventually*, including things happening for large enough ``n : ℕ``, or near enough a point ``x``, or for close enough pairs of points, or things happening almost everywhere in the sense of measure theory. Dually, filters can also express the idea of *things happening often*: for arbitrarily large ``n``, or at a point in any neighborhood of given a point etc...
 
-The examples of filters appearing in these descriptions will be described in details later, but we can already name them:
+The examples of filters appearing in these descriptions will be described in detail later, but we can already name them:
 
 * ``(at_top : filter ℕ)`` : made of sets of ``ℕ`` containing ``{n | n ≥ N}`` for some ``N``
 * ``𝓝 x`` : made of neighborhoods of ``x`` in a topological space
 * ``𝓤 X`` : made of entourages of a uniform space (those space are generalizations of metric spaces and topological groups)
 * ``μ.a_e`` : made of sets whose complement has zero measure with respect to a measure ``μ``.
 
-The general definition is as follows. A filter ``F : filter X`` is a
+The general definition is as follows: a filter ``F : filter X`` is a
 collection of sets ``F.sets : set (set X)`` such that
 
 * ``F.univ_sets : univ ∈ F.sets`` (the set of all inhabitants of ``X`` belongs to ``F.sets``)
-* ``F.sets_of_superset : ∀ {U V}, U ∈ F.sets → U ⊆ V → V ∈ F.sets`` (if ``U`` belong to ``F.sets`` then anything larger also belongs)
+* ``F.sets_of_superset : ∀ {U V}, U ∈ F.sets → U ⊆ V → V ∈ F.sets`` (if ``U`` belongs to ``F.sets`` then anything larger also does)
 * ``F.inter_sets : ∀ {U V}, U ∈ sets → V ∈ sets → U ∩ V ∈ sets`` (``F.sets`` is stable under intersection)
 
 In mathlib, ``F`` is defined as a structure bundling ``F.sets`` and its
@@ -40,13 +40,13 @@ However the word sets still appears in some lemma names.
 
 One can see from the axioms above that each filter defines a notion of "sufficiently large" set: the first
 condition says that ``univ`` is sufficiently large, the second one says that a set containing a sufficiently 
-large set is sufficient large and the third one says that the intersection of two sufficiently large sets 
+large set is sufficiently large and the third one says that the intersection of two sufficiently large sets 
 is sufficiently large. However a more useful way to see filters is that 
 filters on a type ``X`` can be seen as generalized elements of ``set X``. For instance ``at_top`` is the 
 "set of very large numbers", and ``𝓝 x₀`` is the "set of points very close to ``x₀``".
 A first precise statement is that one can turn any ``s : set X`` into the so-called principal filter
 associated to ``s``. This definition is already in mathlib and has a notation ``𝓟`` (localized in ``filter``),
-but here we use as an opportunity to construct a filter.
+but for demonstration purposes we use this as an opportunity to explicitly construct a filter:
 TEXT. -/
 
 -- QUOTE:
@@ -105,8 +105,8 @@ A neighborhood of ``x`` is a set containing an open interval ``Ioo (x - ε) (x +
 (of course this is only a special case of a more general construction in mathlib).
 
 
-With those examples, we can already define what is means for a function ``f : X → Y``
-to converges with respect to some ``G : filter Y`` along some ``F : filter X`` as
+With these examples, we can already define what is means for a function ``f : X → Y``
+to converge with respect to some ``G : filter Y`` along some ``F : filter X`` as
 TEXT. -/
 -- QUOTE:
 def tendsto₁ {X Y : Type*} (f : X → Y) (F : filter X) (G : filter Y) := 
@@ -119,7 +119,7 @@ converges to the real number ``x``. When both ``X`` and ``Y`` are ``ℝ``, ``ten
 is equivalent to :math:`\lim_{x \to x₀} f(x) = y₀`. All other kinds of limits mentioned in the introduction are
 also equivalent to ``tendsto₁`` for some filters on source and target.
 
-The definition quoted above is definitionaly equivalent to what is in mathlib, but it does not appear in this way,
+The definition quoted above is definitionally equivalent to what is in mathlib, but it does not appear in this way,
 hence the subscript in the name.
 The issue with the above definition is that it exposes a quantifier and elements of ``G``, and it hides the intuition
 coming from seeing filters as generalized sets. It is more efficient to
@@ -127,7 +127,7 @@ hide the ``∀ V`` and expose the intuition using more algebraic structure.
 The first ingredient is the push-forward operation :math:`f_*` associated to any map ``f : X → Y``:
 ``filter.map f``. Given a filter ``F`` on ``X``, we get ``filter.map f F : filter Y`` and, definitionnaly,
 ``V ∈ filter.map f F ↔ f ⁻¹' V ∈ F``. In this file we've opened the filter namespace, so
-``filter.map`` can be written as ``map``. This allows to rewrite the definition of ``tendsto`` using
+``filter.map`` can be written as ``map``. This allows rewriting the definition of ``tendsto`` using
 the order relation on ``filter Y`` which is reversed inclusion of the set of members:
 given ``G H : filter Y``, ``G ≤ H ↔ ∀ V : set Y, V ∈ H → V ∈ G``.
 
@@ -153,7 +153,7 @@ We can now understand intuitively why a sequence ``u : ℕ → ℝ`` converges t
 a point ``x₀`` if and only if ``map u at_top ≤ 𝓝 x₀``: this inequality means the "direct image under u" of 
 "the set of very big natural numbers" is "included" in "the set of points very close to ``x₀``".
 
-As promised, the above formulation exhibits no quantifier and no set. It also allows to leverage algebraic property
+As promised, the above formulation exhibits no quantifier and no set. It also allows leveraging the algebraic property
 of the push-forward operation. First each ``filter.map f`` is monotone. And then ``filter.map`` is compatible with
 composition.
 TEXT. -/
@@ -164,8 +164,8 @@ TEXT. -/
 -- QUOTE.
 
 /- TEXT:
-Together these two properties allow to prove that limits compose, covering in one shot all 2197 variants from the introduction.
-You can practice proving this statement using either the definition involving the quantifier or the algebraic definition
+Together these two properties allow us to prove that limits compose, covering in one shot all 2197 variants from the introduction.
+You can practice proving the following statement using either the definition involving the quantifier or the algebraic definition
 together with the two above lemmas.
 TEXT. -/
 -- QUOTE:
@@ -193,9 +193,9 @@ end
 /- TEXT:
 
 The push-forward construction uses a map to push filters from the map source to the map target. 
-There also a pull-back operation going in the other direction: ``filter.comap`` and generalizing the
+There also a pull-back operation going in the other direction: ``filter.comap``, generalizing the
 preimage operation on sets. For any given map ``f``,
-``filter.map f`` and ``filter.comap f`` form a Galois connexion: 
+``filter.map f`` and ``filter.comap f`` form a Galois connection: 
 ``filter.map_le_iff_le_comap : filter.map f F ≤ G ↔ F ≤ filter.comap f G``. 
 In particular this operation could be used to provided another formulation of ``tendsto`` that would be propositionally
 (but not definitionaly) equivalent to the existing one.
@@ -223,7 +223,7 @@ TEXT. -/
 
 /- TEXT:
 Let's now move up to the plane ``ℝ × ℝ`` and try to understand how neighborhoods of some point
-``(x₀, y₀)`` are related to ``𝓝 x₀`` and ``𝓝 y₀``. There is product operation 
+``(x₀, y₀)`` are related to ``𝓝 x₀`` and ``𝓝 y₀``. There is a product operation 
 ``filter.prod : filter X → filter Y → filter (X × Y)``, denoted by ``×ᶠ`` which answers this question:
 TEXT. -/
 -- QUOTE:
@@ -234,10 +234,10 @@ example : 𝓝 (x₀, y₀) = 𝓝 x₀ ×ᶠ 𝓝 y₀ := nhds_prod_eq
 This operation is defined in term of the pull-back operation and the ``inf`` operation:
 ``F ×ᶠ G = (comap prod.fst F) ⊓ (comap prod.snd G)``.
 Here the ``inf`` operation refers to the lattice structure on ``filter X`` for any type ``X``:
-``F ⊓ G`` is the greatest filter that is smaller than bother ``F`` and ``G``, generalizing the notion
+``F ⊓ G`` is the greatest filter that is smaller than both ``F`` and ``G``, generalizing the notion
 of the intersection of sets.
 
-A lot of proofs in  mathlib use all this structure (``map``, ``comap``, ``inf``, ``sup``, ``prod``) 
+A lot of proofs in mathlib use all of the aforementioned structure (``map``, ``comap``, ``inf``, ``sup``, ``prod``) 
 to give algebraic proofs about convergence without ever referring to members of filters.
 You can practice this on the following lemma, unfolding the definition of ``tendsto``
 and ``filter.prod`` if needed.
@@ -276,7 +276,7 @@ The ordered type ``filter X`` is actually a *complete* lattice: there is a botto
 any set of filters on ``X`` have an ``Inf`` and a ``Sup``.
 
 Note that in the definition of filters, given the second property
-(if ``U`` belong to ``F`` then anything larger also belongs), the first property 
+(if ``U`` belongs to ``F`` then anything larger also belongs), the first property 
 (the set of all inhabitants of ``X`` belongs to ``F``) is
 equivalent to asking that ``F`` is not the empty collection of sets.
 This shouldn't be confused with a more subtle question. The 
@@ -303,7 +303,7 @@ a type class ``filter.ne_bot``, so you sometimes see lemmas assuming
 and knows that pushing forward a non-trivial filter gives a non-trivial filter, so a lemma
 assuming ``[F.ne_bot]`` will automatically apply to ``map u at_top`` for any sequence ``u``.
 
-Our tour of algebraic properties of filters and their relations to limit is essentially done,
+Our tour of algebraic properties of filters and their relation to limits is essentially done,
 but careful readers may have noticed that our claim that we get back the usual notions
 of limits is not so clear. Superficially, it may look like ``tendsto u at_top (𝓝 x₀)``
 is stronger than the usual condition because we ask that *every* neighborhood of ``x₀`` 
@@ -325,7 +325,7 @@ nhds_basis_Ioo_pos x₀
 
 /- TEXT:
 Of course we also have a nice basis for ``at_top``, and the lemma ``filter.has_basis.tendsto_iff`` allows
-to reformulate a ``tendsto f F G`` given bases for ``F`` and ``G``.
+us to reformulate a ``tendsto f F G`` given bases for ``F`` and ``G``.
 TEXT. -/
 
 -- QUOTE:
@@ -339,8 +339,8 @@ end
 -- QUOTE.
 
 /- TEXT:
-We now turn to the way filters allow to efficiently handle properties that holds for sufficiently large numbers
-or sufficiently close to a given point. If  you did the exercices in :numref:`sequences_and_convergence`,
+We now turn to the way filters allow to efficiently handle properties that hold for sufficiently large numbers
+or sufficiently close to a given point. If you did the exercises in :numref:`sequences_and_convergence`,
 you have very often been in the situation where some property ``P n`` holds for sufficiently large ``n`` and some
 other property ``Q n`` holds for sufficiently large ``n``. After two ```cases`` this gave you
 ``N_P`` and ``N_Q`` such that ``∀ n ≥ N_P, P n`` and ``∀ n ≥ N_Q, Q n``. Then you ``set N := max N_P N_Q`` and
@@ -348,7 +348,7 @@ patiently proved that ``∀ n ≥ N, P n ∧ Q n``. After doing this a couple of
 
 The way to solve this issue is to note that "``P n`` and ``Q n`` hold for large enough ``n``" means 
 ``{n | P n} ∈ at_top`` and ``{n | Q n} ∈ at_top``.
-Then the fact ``at_top`` is a filter means in particular that the intersection of two elements of ``at_top`` 
+Then the fact that ``at_top`` is a filter means in particular that the intersection of two elements of ``at_top`` 
 is again in ``at_top``, so ``{n | P n ∧ Q n} ∈ at_top``. However writing ``{n | P n} ∈ at_top`` is not very nice.
 In this situation, we use the more suggestive notation ``∀ᶠ n in at_top, P n``. 
 Note the superscript ``f`` which stands for filter. 
@@ -369,7 +369,7 @@ when ``P`` is an equality or inequality statement. For instance, let ``u`` and `
 two sequences of real numbers. Let us state that if
 ``u n`` and ``v n`` coincide for large enough ``n`` then ``u`` tends to ``x₀`` if and only
 if ``v`` tends to ``x₀``. First we'll use the generic ``eventually`` and then the one 
-specialized ``eventually_eq`` for the equality predicate. Note that both statement are
+specialized for the equality predicate, ``eventually_eq`` . Note that both statements are
 definitionaly equivalent so the same proof will be used.
 
 TEXT. -/
@@ -404,7 +404,7 @@ TEXT. -/
 
 /- TEXT:
 The item above, corresponding to ``eventually.mono`` is also a very nice use of filters, especially combined
-with ``eventually.and``. There is also a tactic ``filter_upwards`` that allows to nicely combines these. 
+with ``eventually.and``. The ``filter_upwards`` tactic allows us to nicely combine them. 
 Compare:
 
 TEXT. -/
@@ -437,7 +437,7 @@ Readers who know about measure theory will note that the filter ``μ.ae`` of set
 (aka "the set of almost every point") is not very useful as the source or target of ``tendsto`` but very often
 used with ``eventually``, when saying a property holds for almost every point.
 
-There is dual version of ``∀ᶠ x in F, P x`` which is much less useful, but still exists: ``∃ᶠ x in F, P x`` means
+There is a dual version of ``∀ᶠ x in F, P x`` which is much less useful, but still exists: ``∃ᶠ x in F, P x`` means
 ``{x | ¬P x} ∉ f``. For instance ``∃ᶠ n in at_top, P n`` means there are arbitrarily large ``n`` such that ``P n``.
 This notation stands for ``filter.frequently``.
 
