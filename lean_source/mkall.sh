bc8e5c367c98c3dbd4d1cdd251d921af44c2f75c
Patrick Massot <patrickmassot@free.fr>
2022-07-12 20:52:34 +0200

    Add a topological space section.

--- lean_source/mkall.sh
+++ lean_source/mkall.sh
@@ -21,3 +21,4 @@ lean_source/mkdoc.py 06_Abstract_Algebra 01_Structures
 lean_source/mkdoc.py 06_Abstract_Algebra 02_Algebraic_Structures
 lean_source/mkdoc.py 07_Topology 01_Filters
 lean_source/mkdoc.py 07_Topology 02_Metric_Spaces
+lean_source/mkdoc.py 07_Topology 03_Topological_Spaces
