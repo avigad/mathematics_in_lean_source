47d11d78e6fe5d1189be974b3ed24871c01f21e6
Julian Berman <Julian@GrayVines.com>
2023-07-01 19:13:17 +0200

    Also remove an unused variable.

--- MIL/C02_Basics/Source_S01_Calculating.lean
+++ MIL/C02_Basics/Source_S01_Calculating.lean
@@ -219,7 +219,7 @@ TEXT. -/
 section
 
 -- QUOTE:
-variable (a b c d e f g : ℝ)
+variable (a b c d e f : ℝ)
 
 example (h : a * b = c * d) (h' : e = f) : a * (b * e) = c * (d * f) := by
   rw [h', ← mul_assoc, h, mul_assoc]
