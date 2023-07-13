2892a3d5b212bd02e4dcbd0ab9cc12b16294c0e7
Floris van Doorn <fpvdoorn@gmail.com>
2023-07-13 11:43:25 +0200

    a few more

--- MIL/C09_Differential_Calculus/Source_S01_Elementary_Differential_Calculus.lean
+++ MIL/C09_Differential_Calculus/Source_S01_Elementary_Differential_Calculus.lean
@@ -30,7 +30,7 @@ example : HasDerivAt sin 1 0 := by simpa using hasDerivAt_sin 0
 /- TEXT:
 We can also express that ``f`` is differentiable at a point without
 specifying its derivative there
-by writing ``differentiable_at ℝ``.
+by writing ``DifferentiableAt ℝ``.
 We specify ``ℝ`` explicitly because in a slightly more general context,
 when talking about functions from ``ℂ`` to ``ℂ``,
 we want to be able to distinguish between being differentiable in the real sense
