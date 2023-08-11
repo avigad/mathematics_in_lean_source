53bcc27fdcb0f31e8e5eccb8eaa97c68d9a29527
Jeremy Avigad <avigad@cmu.edu>
2023-08-11 17:27:48 -0400

    fix test script

--- scripts/examples_test.py
+++ scripts/examples_test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-from mkdoc import make_everything, make_exercises_test
+from mkdoc import make_everything, make_examples_test
 
 if __name__ == '__main__':
     make_everything()
-    make_exercises_test()
+    make_examples_test()
