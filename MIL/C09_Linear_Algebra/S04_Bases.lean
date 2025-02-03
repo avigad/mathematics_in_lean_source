343d88aefc9b2d8649d12ca66681667dfee53321
LessnessRandomness <lessnessr@gmail.com>
2025-02-03 12:16:41 +0200

    Tiny fix in S04_Bases.lean
    Fixed an example so that it is about vector sum, not matrix sum.

--- MIL/C09_Linear_Algebra/S04_Bases.lean
+++ MIL/C09_Linear_Algebra/S04_Bases.lean
@@ -34,7 +34,7 @@ EXAMPLES: -/
 section matrices
 
 -- Adding vectors
-#eval !![1, 2] + !![3, 4]  -- !![4, 6]
+#eval ![1, 2] + ![3, 4]  -- ![4, 6]
 
 -- Adding matrices
 #eval !![1, 2; 3, 4] + !![3, 4; 5, 6]  -- !![4, 6; 8, 10]
